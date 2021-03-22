881c3b4e85a81f90f3eba685121a7d2b0d248292
codessbabe <oana.anghelachi@gmail.com>
2021-03-22 05:11:27 +0100

    Add 'context' in class-component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-// import React from 'react'
+import React, { useState } from "react"
 import "./App.css"
 import Counter from "./Counter"
 import CounterHooks from "./CounterHooks"
 
+export const ThemeContext = React.createContext()
 function App() {
+  const [theme, setTheme] = useState("red")
+
   return (
-    <div className="App">
-      {/* adding props to counter */}
-      Counter
-      <Counter initialCount={0} />
-      Counter Hooks
-      <CounterHooks initialCount={3} />
-      <figure>
-        <blockquote>
-          “An investment in knowledge pays the best interest.”
-        </blockquote>
-        <figcaption>- Benjamin Franklin</figcaption>
-      </figure>
-    </div>
+    <ThemeContext.Provider value={{backgroundColor: theme}}>
+      <div className="App">
+        {/* adding props to counter */}
+        Counter
+        <Counter initialCount={0} />
+        Counter Hooks
+        <CounterHooks initialCount={3} />
+        <button onClick={()=> setTheme(prevTheme => { 
+          return prevTheme === 'red' ? 'blue' : 'red'
+        })}></button>
+        <figure>
+          <blockquote>
+            “An investment in knowledge pays the best interest.”
+          </blockquote>
+          <figcaption>- Benjamin Franklin</figcaption>
+        </figure>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
